1447321e73b36f07a3ab6d60b637b22bf8cb9453
agrad0 <110928774+agrad0@users.noreply.github.com>
2022-11-17 20:57:59 +0100

    17.11

---
+++ js/main.js
@@ -0,0 +1,40 @@
+// $(document).ready(function() {
+//     (console.log(`działa`));
+//     $("#sm-container1").toggleClass(".sm-container-shown"[true, 10000]);
+//     (console.log(`zadziałało hehe`));
+// })
+
+
+// $("#team-img-1").mouseenter(function() {
+//     $(`#footer-expanding1`).animate({bottom: `10%`}, 500, function(){
+//         $(`#footer-expanding1`).addClass(`box-shadow`);
+//     })
+//     $('#sm-container1').show().css({"height":'0%'}).animate({"height":'100%'},300);
+// });
+
+// $("#team-img-1").mouseleave(function() {
+//     $("#footer-expanding1").animate({bottom: `-15%`}, 300)
+//     $('#sm-container1').show().css({"height":'100%'}).animate({"height":'0%'},300, function (){
+//     $('#sm-container1').hide();
+//     $(`#footer-expanding1`).removeClass(`box-shadow`);
+//     });
+// });
+
+$(".img-item2").mouseenter(function() {
+    $(this).children(':nth-child(2)').animate({bottom: `0%`}, 500, function(){
+        $(this).addClass(`box-shadow`);
+    })
+    $(this).find(`.sm-container`).show().css({"height":'0%'}).animate({"height":'100%'},300);
+});
+
+$(".img-item2").mouseleave(function() {
+    $(this).children(':nth-child(2)').animate({bottom: `-15%`}, 500)
+    $(this).find(`.sm-container`).show().css({"height":'100%'}).animate({"height":'0%'},300, function (){
+        $(this).hide();
+        $(this).parent().removeClass(`box-shadow`);
+    });
+});
+
+$(`.img-item2`).mouseenter(function(event) {
+    event.stopPropagation();
+});
